91b6dbb4e44394c5635313e2a7b7f1883b42c74e
Ben Lönnqvist <lonnqvistben@gmail.com>
2021-01-09 16:10:03 +0200

    fix to work with isolatedModules

--- mod.ts
+++ mod.ts
@@ -5,21 +5,236 @@ export type {
   RunWithWebhookParams,
 } from "./src/telegram-bot/run-params.ts";
 
-export * from "./src/types/common/methods.ts";
-export * from "./src/types/common/objects.ts";
-export * from "./src/types/games/methods.ts";
-export * from "./src/types/games/objects.ts";
-export * from "./src/types/getting-updates/methods.ts";
-export * from "./src/types/getting-updates/objects.ts";
-export * from "./src/types/getting-updates/update-helpers.ts";
-export * from "./src/types/inline-mode/methods.ts";
-export * from "./src/types/inline-mode/objects.ts";
-export * from "./src/types/passport/methods.ts";
-export * from "./src/types/passport/objects.ts";
-export * from "./src/types/payments/methods.ts";
-export * from "./src/types/payments/objects.ts";
-export * from "./src/types/stickers/methods.ts";
-export * from "./src/types/stickers/objects.ts";
-export * from "./src/types/updating-messages/methods.ts";
-export * from "./src/types/method.ts";
-export * from "./src/types/utils.ts";
+export type {
+  AnswerCallbackQuery,
+  Close,
+  CopyMessage,
+  DeleteChatPhoto,
+  DeleteChatStickerSet,
+  EditMessageLiveLocation,
+  ExportChatInviteLink,
+  ForwardMessage,
+  GetChat,
+  GetChatAdministrators,
+  GetChatMember,
+  GetChatMembersCount,
+  GetFile,
+  GetMe,
+  GetMyCommands,
+  GetUserProfilePhotos,
+  KickChatMember,
+  LeaveChat,
+  PinChatMessage,
+  PromoteChatMember,
+  RestrictChatMember,
+  SendAnimation,
+  SendAudio,
+  SendChatAction,
+  SendContact,
+  SendDice,
+  SendDocument,
+  SendLocation,
+  SendMediaGroup,
+  SendMessage,
+  SendPhoto,
+  SendPoll,
+  SendVenue,
+  SendVideo,
+  SendVideoNote,
+  SendVoice,
+  SetChatAdministratorCustomTitle,
+  SetChatDescription,
+  SetChatPermissions,
+  SetChatPhoto,
+  SetChatStickerSet,
+  SetChatTitle,
+  SetMyCommands,
+  StopMessageLiveLocation,
+  UnbanChatMember,
+  UnpinAllChatMessages,
+  UnpinChatMessage,
+} from "./src/types/common/methods.ts";
+export type {
+  Animation,
+  Audio,
+  BotCommand,
+  CallbackQuery,
+  Chat,
+  ChatExtended,
+  ChatLocation,
+  ChatMember,
+  ChatPermissions,
+  ChatPhoto,
+  Contact,
+  Dice,
+  Document,
+  File,
+  ForceReply,
+  InlineKeyboardButton,
+  InlineKeyboardCallbackDataButton,
+  InlineKeyboardCallbackGameButton,
+  InlineKeyboardLoginUrlButton,
+  InlineKeyboardMarkup,
+  InlineKeyboardPayButton,
+  InlineKeyboardSwitchInlineQueryButton,
+  InlineKeyboardSwitchInlineQueryCurrentChatButton,
+  InlineKeyboardUrlButton,
+  InputFile,
+  InputMedia,
+  InputMediaAnimation,
+  InputMediaAudio,
+  InputMediaDocument,
+  InputMediaPhoto,
+  InputMediaVideo,
+} from "./src/types/common/objects.ts";
+export type {
+  GetGameHighScores,
+  SendGame,
+  SetGameScore,
+} from "./src/types/games/methods.ts";
+export type {
+  CallbackGame,
+  Game,
+  GameHighScore,
+} from "./src/types/games/objects.ts";
+export type {
+  DeleteWebhook,
+  GetUpdates,
+  GetWebhookInfo,
+  SetWebhook,
+} from "./src/types/getting-updates/methods.ts";
+export type {
+  Update,
+  WebhookInfo,
+} from "./src/types/getting-updates/objects.ts";
+export type {
+  CallbackQueryUpdate,
+  ChannelPostUpdate,
+  ChosenInlineResultUpdate,
+  EditedChannelPostUpdate,
+  EditedMessageUpdate,
+  InlineQueryUpdate,
+  MessageUpdate,
+  PollAnswerUpdate,
+  PollUpdate,
+  PreCheckoutQueryUpdate,
+  ShippingQueryUpdate,
+  UpdateError,
+} from "./src/types/getting-updates/update-helpers.ts";
+export {
+  CallbackQueryUpdateEvent,
+  ChannelPostUpdateEvent,
+  ChosenInlineResultUpdateEvent,
+  EditedChannelPostUpdateEvent,
+  EditedMessageUpdateEvent,
+  InlineQueryUpdateEvent,
+  isCallbackQueryUpdate,
+  isChannelPostUpdate,
+  isChosenInlineResultUpdate,
+  isEditedChannelPostUpdate,
+  isEditedMessageUpdate,
+  isErrorUpdateEvent,
+  isInlineQueryUpdate,
+  isMessageUpdate,
+  isPollAnswerUpdate,
+  isPollUpdate,
+  isPreCheckoutQueryUpdate,
+  isShippingQueryUpdate,
+  MessageUpdateEvent,
+  PollAnswerUpdateEvent,
+  PollUpdateEvent,
+  PreCheckoutQueryUpdateEvent,
+  ShippingQueryUpdateEvent,
+  UpdateErrorEvent,
+  UpdateEvent,
+  UpdateType,
+} from "./src/types/getting-updates/update-helpers.ts";
+export type { AnswerInlineQuery } from "./src/types/inline-mode/methods.ts";
+export type {
+  ChosenInlineResult,
+  InlineQuery,
+  InlineQueryResult,
+  InlineQueryResultArticle,
+  InlineQueryResultAudio,
+  InlineQueryResultCachedAudio,
+  InlineQueryResultCachedDocument,
+  InlineQueryResultCachedGif,
+  InlineQueryResultCachedMpeg4Gif,
+  InlineQueryResultCachedPhoto,
+  InlineQueryResultCachedSticker,
+  InlineQueryResultCachedVideo,
+  InlineQueryResultCachedVoice,
+  InlineQueryResultContact,
+  InlineQueryResultDocument,
+  InlineQueryResultGame,
+  InlineQueryResultGif,
+  InlineQueryResultLocation,
+  InlineQueryResultMpeg4Gif,
+  InlineQueryResultPhoto,
+  InlineQueryResultVenue,
+  InlineQueryResultVideo,
+  InlineQueryResultVoice,
+  InputContactMessageContent,
+  InputLocationMessageContent,
+  InputMessageContent,
+  InputTextMessageContent,
+  InputVenueMessageContent,
+} from "./src/types/inline-mode/objects.ts";
+export type { SetPassportDataErrors } from "./src/types/passport/methods.ts";
+export type {
+  EncryptedCredentials,
+  EncryptedPassportElement,
+  PassportData,
+  PassportElementError,
+  PassportElementErrorDataField,
+  PassportElementErrorFile,
+  PassportElementErrorFiles,
+  PassportElementErrorFrontSide,
+  PassportElementErrorReverseSide,
+  PassportElementErrorSelfie,
+  PassportElementErrorTranslationFile,
+  PassportElementErrorTranslationFiles,
+  PassportElementErrorUnspecified,
+  PassportFile,
+} from "./src/types/passport/objects.ts";
+export type {
+  AnswerPreCheckoutQuery,
+  AnswerShippingQuery,
+  SendInvoice,
+} from "./src/types/payments/methods.ts";
+export type {
+  Invoice,
+  LabeledPrice,
+  OrderInfo,
+  PreCheckoutQuery,
+  ShippingAddress,
+  ShippingOption,
+  ShippingQuery,
+  SuccessfulPayment,
+} from "./src/types/payments/objects.ts";
+export type {
+  AddStickerToSet,
+  CreateNewStickerSet,
+  DeleteStickerFromSet,
+  GetStickerSet,
+  SendSticker,
+  SetStickerPositionInSet,
+  SetStickerSetThumb,
+  UploadStickerFile,
+} from "./src/types/stickers/methods.ts";
+export type {
+  MaskPosition,
+  Sticker,
+  StickerSet,
+} from "./src/types/stickers/objects.ts";
+export type {
+  DeleteMessage,
+  EditMessageCaption,
+  EditMessageMedia,
+  EditMessageReplyMarkup,
+  EditMessageText,
+  StopPoll,
+} from "./src/types/updating-messages/methods.ts";
+export type { Method } from "./src/types/method.ts";
+export type { Attachments } from "./src/types/utils.ts";
+export { DiceEmoji } from "./src/types/utils.ts";
